e3d07ccd66358f59bb6acd0af6d59584ec09f7da
KazuhisaTaniguchi <jjzoow96@gmail.com>
2016-02-21 17:49:46 +0900

    done 22
    from 23

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,12 +1,61 @@
 from django.shortcuts import render
-from .forms import SingUpForm
+from django.core.mail import send_mail
+from django.conf import settings
+from .forms import (
+    SingUpForm,
+    ContactForm,
+)
 
 
 def home(request):
     title = 'Wellcome'
-    if request.user.is_authenticated():
-        title = "My Title %s" % (request.user)
+    form = SingUpForm(request.POST or None)
     context = {
-        'template_title': title,
+        'title': title,
+        'form': form,
     }
+
+    if form.is_valid():
+        # print request.POST['email'] not recommended
+        instance = form.save(commit=False)
+
+        full_name = form.cleaned_data.get('full_name')
+        if not full_name:
+            full_name = 'New full name'
+        instance.full_name = full_name
+
+        instance.save()
+        context = {
+            'title': 'Thank you'
+        }
     return render(request, 'newsletter/home.html', context)
+
+
+def contact(request):
+    form = ContactForm(request.POST or None)
+
+    if form.is_valid():
+        # for key, value in form.cleaned_data.iteritems():
+        #     print key, value
+        form_email = form.cleaned_data.get('email')
+        form_message = form.cleaned_data.get('message')
+        form_full_name = form.cleaned_data.get('full_name')
+
+        subject = 'Site contact form'
+        from_email = settings.EMAIL_HOST_USER
+        to_email = form_email
+        contact_message = '%s: %s via %s' % (
+            form_full_name, form_message, form_email)
+
+        send_mail(
+            subject,
+            contact_message,
+            from_email,
+            [to_email],
+            fail_silently=False,
+        )
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'newsletter/forms.html', context)
